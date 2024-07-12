9ecbf515cf5a7a4349896f2f7f506d829bc6b223
SOUMYADEEP PAUL <155042736+Soumyadeep-4704@users.noreply.github.com>
2024-07-12 21:02:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+//Hamburegr Menu for small sized screens
+
+
+var sidebarToggler = document.querySelector(".sidebar");
+
+function showSidebar(){
+    sidebarToggler.style.display = "flex";
+}
+
+function closeSidebar(){
+    sidebarToggler.style.display = "none";
+}
